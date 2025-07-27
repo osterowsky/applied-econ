0c0ff6f6f9da8f7028117920fcde5be05631dfc7
osterowsky <ostern.patryk02@gmail.com>
2025-07-27 14:47:41 +0200

    minor: center items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import QuestionModel from './components/QuestionModel'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div id="app">
+      <h1>Applied Economics</h1>
+      <QuestionModel />
+    </div>
   )
 }
 
---
+++ src/components/QuestionModel.tsx
@@ -0,0 +1,7 @@
+export default function QuestionModel() {
+  return (
+    <div className="question-model">
+      <h2>Question Model Component</h2>
+    </div>
+  );
+}
